5555e9cf61021242dc71127cedd259e4dbe7afc7
BytefulRashi <rashimishra4219@gmail.com>
2025-03-21 02:44:40 +0530

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+const connection = mongoose.createConnection(proccess.env.db_link).on('open',()=>{
+    console.log('mongodb connected');
+}).on('error',()=>{
+    console.log('mongodb connection error');
+});
+
+module.exports = connection;
